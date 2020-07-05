c77f3132ccd3cb96cf0f768b8e35a74c74aba9d3
Your Name In Single Quotes <your email here in single quotes>
2020-07-05 15:08:40 +0300

    add

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var userName = prompt('What is your name');   
+console.log("The user's name is "+userName);
+var CatName = prompt('What is your cat\'s name?');
+console.log("The cat's name is "+CatName);
+var catFood = prompt ('what do you feed your cat?')
+console.log("The cat's food is "+catFood);
+alert('Ooh ' + userName + '! ' + catFood + ' is a good food for ' + CatName);
+var CatColor = prompt ('what is the color of your cat?')
+console.log("The cat color is "+CatColor);
+alert (CatColor + ' is a cute color! I hope you\'ll enjoy in our website')
